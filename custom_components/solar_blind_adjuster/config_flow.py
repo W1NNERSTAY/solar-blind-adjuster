ba80ab30629fe30c4d5730c7b71f6e0bd9ecb545
ohilseung <ohilseung@gmail.com>
2025-12-14 02:44:11 +0900

    Fix options flow base init

--- custom_components/solar_blind_adjuster/config_flow.py
+++ custom_components/solar_blind_adjuster/config_flow.py
@@ -310,7 +310,8 @@ class SolarBlindAdjusterOptionsFlow(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: config_entries.ConfigEntry):
         """Initialize options flow."""
-        super().__init__(config_entry)
+        super().__init__()
+        self.config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
